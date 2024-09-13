3c17e3c3851f1b5c9882e01004c8984e2ed22f90
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-13 21:34:56 +0700

    2.4. Kết nối database

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDatabase } from "./config/database";
+connectDatabase();
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
