1a17e2c87c13904c9adf78ff4aa1838fcf27b8e9
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-14 07:07:59 +0700

    2.5. Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -5,13 +5,21 @@ dotenv.config();
 import { connectDatabase } from "./config/database";
 connectDatabase();
 
+import Topic from "./models/topic.model";
+
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    });
+
+    console.log(topics);
+
     res.render("client/pages/topics/index");
 });
 
