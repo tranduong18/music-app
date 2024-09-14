acbcc8ed41dee61df8a1827021ec37235a61166e
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-14 07:44:05 +0700

    5. Làm trang danh sách bài hát (bên client)

---
+++ controllers/client/song.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+
+// [GET] /songs/:slugTopic
+export const list = async (req: Request, res: Response) => {
+    const slugTopic: string = req.params.slugTopic;
+
+    const topic = await Topic.findOne({
+        slug: slugTopic,
+        deleted: false,
+        status: "active"
+    });
+
+    const songs = await Song.find({
+        topicId: topic.id,
+        deleted: false,
+        status: "active"
+    }).select("title avatar singerId like slug");
+
+    for(const item of songs){
+        const singerInfo = await Singer.findOne({
+            _id: item.singerId
+        }).select("fullName");
+
+        item["singerFullName"] = singerInfo["fullName"];
+    }
+
+    res.render("client/pages/songs/list", {
+        pageTitle: topic.title,
+        songs: songs
+    });
+}
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,9 @@
 import { Express } from "express";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 
 export const routesClient = (app: Express) => {
     app.use("/topics", topicRoute);
+
+    app.use("/songs", songRoute);
 }
