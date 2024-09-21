e41812631da9e84a89d78f69520ae62ec1b95af7
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-21 23:18:48 +0700

    5.1. Làm giao diện

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import { connectDatabase } from "./config/database";
 import { routesAdmin } from "./routes/admin/index.route";
 import { routesClient} from "./routes/client/index.route";
 import { systemConfig } from "./config/system";
+import path from "path";
 
 connectDatabase();
 
@@ -21,10 +22,12 @@ app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 routesAdmin(app);
 routesClient(app);
