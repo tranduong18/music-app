47ccdf3e2ca0a32b5160ff30fa2f45618539825f
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-21 23:08:28 +0700

    4. Tạo trang Danh sách bài hát (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { dashboardRoute } from "./dashboard.route";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 import { systemConfig } from "../../config/system";
 
 export const routesAdmin = (app : Express) => {
@@ -9,4 +10,6 @@ export const routesAdmin = (app : Express) => {
     app.use(`${path}/dashboard`, dashboardRoute);
 
     app.use(`${path}/topics`, topicRoute);
+
+    app.use(`${path}/songs`, songRoute);
 }
