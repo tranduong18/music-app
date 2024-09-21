e5ee6b62da7854b07e0f9d272bcc93204a7c9a11
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-09-21 23:04:40 +0700

    3. Trang Quản lý chủ đề (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
 import { dashboardRoute } from "./dashboard.route";
+import { topicRoute } from "./topic.route";
 import { systemConfig } from "../../config/system";
 
 export const routesAdmin = (app : Express) => {
     const path = `/${systemConfig.prefixAdmin}`;
 
     app.use(`${path}/dashboard`, dashboardRoute);
+
+    app.use(`${path}/topics`, topicRoute);
 }
